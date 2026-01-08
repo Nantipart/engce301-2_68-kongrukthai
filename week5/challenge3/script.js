ad39c82122dd417ffc11d7254f077c27b41d2e71
nantipart <jamelopesnan@gmail.com>
2026-01-08 14:18:51 +0700

    13

--- week5/challenge3/script.js
+++
@@ -1,9 +0,0 @@
-// เลือกลิงก์ทั้งหมดใน nav
-const navLinks = document.querySelectorAll("nav a");
-
-navLinks.forEach(link => {
-    link.addEventListener("click", function (event) {
-        event.preventDefault(); // ไม่ให้กระโดดหน้า
-        alert("คุณคลิก: " + this.textContent);
-    });
-});
